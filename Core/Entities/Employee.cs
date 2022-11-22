92ab1694c7ce7ef6e9ac81e087853a2dcf466e8f
Zeyneb Dzhelil <zizidzhelil@gmail.com>
2022-11-22 16:41:45 +0200

    entities are added

---
+++ Core/Entities/Employee.cs
@@ -0,0 +1,15 @@
+﻿namespace Core.Entities
+{
+    public class Employee
+    {
+        public long Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+
+        public DateTime BirthDate { get; set; }
+    }
+}
---
+++ Core/Entities/Parameter.cs
@@ -0,0 +1,19 @@
+﻿namespace Core.Entities
+{
+    public class Parameter
+    {
+        public int Id { get; set; }
+
+        public int Year { get; set; }
+
+        public double MinThreshold { get; set; }
+
+        public double TotalIncomeTaxPercentage { get; set; }
+
+        public double HealthAndSocialInsurancePercentage { get; set; }
+
+        public double MaxThreshold { get; set; }
+
+        public Employee Employee { get; set; }
+    }
+}
