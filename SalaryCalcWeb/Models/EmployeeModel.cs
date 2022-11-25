98c82a3d569e25fef75a223bb7fc18b3def27765
Zeyneb Dzhelil <zizidzhelil@gmail.com>
2022-11-25 14:01:38 +0200

    web project

---
+++ SalaryCalcWeb/Models/EmployeeModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SalaryCalcWeb.Models
+{
+	public class EmployeeModel
+	{
+        [Required]
+        [StringLength(10, ErrorMessage = "Name is too long.")]
+        public string? FirstName { get; set; }
+
+        [Required]
+        [StringLength(10, ErrorMessage = "Name is too long.")]
+        public string? MiddleName { get; set; }
+
+        [Required]
+        [StringLength(10, ErrorMessage = "Name is too long.")]
+        public string? LastName { get; set; }
+
+        public DateTime BirthDate { get; set; }
+    }
+}
---
+++ SalaryCalcWeb/Services/EmployeeService.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Net.Http.Headers;
+using SalaryCalcWeb.Models;
+using System.Text.Json;
+
+namespace SalaryCalcWeb.Services
+{
+	public class EmployeeService
+	{
+		private readonly IHttpClientFactory _httpClientFactory;
+
+		public EmployeeService(IHttpClientFactory httpClientFactory) =>
+			_httpClientFactory = httpClientFactory;
+
+		public async Task<List<EmployeeModel>> GetEmployyes()
+		{
+			List<EmployeeModel> response = new List<EmployeeModel>();
+
+			var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, "https://localhost:7139/Employee")
+			{
+				Headers =
+				{
+					{ HeaderNames.Accept, "application/json" },
+					{ HeaderNames.UserAgent, "HttpRequestsSample" }
+				}
+			};
+
+			var httpClient = _httpClientFactory.CreateClient();
+			var httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
+
+			if (httpResponseMessage.IsSuccessStatusCode)
+			{
+				using var contentStream = await httpResponseMessage.Content.ReadAsStreamAsync();
+				response = JsonSerializer.Deserialize <List<EmployeeModel>> (contentStream);
+			}
+
+			return response;
+		}
+	}
+}
