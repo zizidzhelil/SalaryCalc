4792005c9d4ccc2467bd9aa4a037857f1908a12c
Zeyneb Dzhelil <zizidzhelil@gmail.com>
2022-11-25 14:01:48 +0200

    service

--- SalaryCalcWeb/Program.cs
+++ SalaryCalcWeb/Program.cs
@@ -1,10 +1,18 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using SalaryCalcWeb;
+using SalaryCalcWeb.Services;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
-builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<EmployeeService>();
+builder.Services.AddHttpClient("Local", httpClient =>
+{
+	httpClient.BaseAddress = new Uri("https://localhost:7139/");
+	httpClient.DefaultRequestHeaders.UserAgent.ParseAdd("SalaryCalcWeb");
+	httpClient.DefaultRequestHeaders.TryAddWithoutValidation("Accept", "application/json");
+});
+
 await builder.Build().RunAsync();
--- SalaryCalcWeb/Services/EmployeeService.cs
+++ SalaryCalcWeb/Services/EmployeeService.cs
@@ -8,29 +8,24 @@ namespace SalaryCalcWeb.Services
 	{
 		private readonly IHttpClientFactory _httpClientFactory;
 
-		public EmployeeService(IHttpClientFactory httpClientFactory) =>
+		public EmployeeService(IHttpClientFactory httpClientFactory)
+		{
 			_httpClientFactory = httpClientFactory;
+		}
 
-		public async Task<List<EmployeeModel>> GetEmployyes()
+		public async Task<List<EmployeeModel>> GetEmployees()
 		{
 			List<EmployeeModel> response = new List<EmployeeModel>();
 
-			var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, "https://localhost:7139/Employee")
-			{
-				Headers =
-				{
-					{ HeaderNames.Accept, "application/json" },
-					{ HeaderNames.UserAgent, "HttpRequestsSample" }
-				}
-			};
+			var httpRequestMessage = new HttpRequestMessage(HttpMethod.Get, "https://localhost:7139/Employee");
 
-			var httpClient = _httpClientFactory.CreateClient();
+			var httpClient = _httpClientFactory.CreateClient("Local");
 			var httpResponseMessage = await httpClient.SendAsync(httpRequestMessage);
 
 			if (httpResponseMessage.IsSuccessStatusCode)
 			{
 				using var contentStream = await httpResponseMessage.Content.ReadAsStreamAsync();
-				response = JsonSerializer.Deserialize <List<EmployeeModel>> (contentStream);
+				response = JsonSerializer.Deserialize<List<EmployeeModel>>(contentStream);
 			}
 
 			return response;
