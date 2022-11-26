b7342eee34a49436726abd282039a8415166b94a
Zeyneb Dzhelil <zizidzhelil@gmail.com>
2022-11-26 11:24:33 +0200

    Empty front-end project

---
+++ src/front-end/SalaryCalcWeb/SalaryCalcWeb/Program.cs
@@ -0,0 +1,17 @@
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+
+namespace SalaryCalcWeb
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("#app");
+            builder.RootComponents.Add<HeadOutlet>("head::after");
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
