40e0e885a1382448b1903f8177b926c712b2abeb
Zeyneb Dzhelil <zizidzhelil@gmail.com>
2022-11-27 11:43:55 +0200

    Fix salary calculation model validator

--- src/backend/DAL/Queries/GetYearParams/GetYearParamsQueryHandler.cs
+++ src/backend/DAL/Queries/GetYearParams/GetYearParamsQueryHandler.cs
@@ -1,27 +1,28 @@
 ﻿using Core.Entities;
 using Core.Queries;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 
 namespace DAL.Queries.GetYearParams
 {
-	public class GetYearParamsQueryHandler : IQueryHandler<GetYearParamsQuery, Parameter>
-	{
-		private readonly ILogger _logger;
-		private readonly SalaryCalcContext _context;
+    public class GetYearParamsQueryHandler : IQueryHandler<GetYearParamsQuery, Parameter>
+    {
+        private readonly ILogger _logger;
+        private readonly SalaryCalcContext _context;
 
-		public GetYearParamsQueryHandler(ILogger<GetYearParamsQueryHandler> logger, SalaryCalcContext context)
-		{
-			_logger = logger;
-			_context = context;
-		}
+        public GetYearParamsQueryHandler(ILogger<GetYearParamsQueryHandler> logger, SalaryCalcContext context)
+        {
+            _logger = logger;
+            _context = context;
+        }
 
-		public async Task<Parameter> HandleAsync(GetYearParamsQuery query, CancellationToken cancellationToken = default)
-		{
+        public async Task<Parameter> HandleAsync(GetYearParamsQuery query, CancellationToken cancellationToken = default)
+        {
             _logger.LogInformation($"Begin class {nameof(GetYearParamsQueryHandler)} and method {nameof(GetYearParamsQueryHandler.HandleAsync)}");
-            Parameter parameter = await _context.Parameters.FindAsync(new object[] { query.Year }, cancellationToken: cancellationToken);
+            Parameter parameter = await _context.Parameters.Where(p => p.Year == query.Year).SingleOrDefaultAsync(cancellationToken);
             _logger.LogInformation($"End class {nameof(GetYearParamsQueryHandler)} and method {nameof(GetYearParamsQueryHandler.HandleAsync)}");
 
             return parameter;
-		}
-	}
+        }
+    }
 }
--- src/backend/Services/Validations/CalculateSalaryRequestModelValidator.cs
+++ src/backend/Services/Validations/CalculateSalaryRequestModelValidator.cs
@@ -3,51 +3,62 @@ using Core.Entities;
 using Core.Queries;
 using Core.Validation;
 using DAL.Queries.GetAllEmployees;
+using DAL.Queries.GetYearParams;
 using Microsoft.Extensions.Logging;
 using Services.Models.CalculateSalaryModels.RequestModels;
 using System.Text.RegularExpressions;
 
 namespace Services.Validations
 {
-	public class CalculateSalaryRequestModelValidator : IValidation<CalculateSalaryRequestModel>
-	{
-		private readonly ILogger _logger;
-		private readonly IQueryHandler<GetAllEmployeesQuery, IList<Employee>> _employeesQueryHandler;
+    public class CalculateSalaryRequestModelValidator : IValidation<CalculateSalaryRequestModel>
+    {
+        private readonly ILogger _logger;
+        private readonly IQueryHandler<GetAllEmployeesQuery, IList<Employee>> _employeesQueryHandler;
+        private readonly IQueryHandler<GetYearParamsQuery, Parameter> _getYearParamsQuery;
 
-		public CalculateSalaryRequestModelValidator(
-			ILogger<CalculateSalaryRequestModelValidator> logger,
-			IQueryHandler<GetAllEmployeesQuery, IList<Employee>> employeesQueryHandler)
-		{
-			_logger = logger;
-			_employeesQueryHandler = employeesQueryHandler;
-		}
+        public CalculateSalaryRequestModelValidator(
+            ILogger<CalculateSalaryRequestModelValidator> logger,
+            IQueryHandler<GetAllEmployeesQuery, IList<Employee>> employeesQueryHandler,
+            IQueryHandler<GetYearParamsQuery, Parameter> getYearParamsQuery)
+        {
+            _logger = logger;
+            _employeesQueryHandler = employeesQueryHandler;
+            _getYearParamsQuery = getYearParamsQuery;
+        }
 
-		public async Task Validate(CalculateSalaryRequestModel model, CancellationToken cancellationToken = default)
-		{
+        public async Task Validate(CalculateSalaryRequestModel model, CancellationToken cancellationToken = default)
+        {
             _logger.LogInformation($"Begin class {nameof(CalculateSalaryRequestModelValidator)} and method {nameof(CalculateSalaryRequestModelValidator.Validate)}");
 
             List<string> errorMessages = new List<string>();
 
-			IList<Employee> employees = await _employeesQueryHandler.HandleAsync(new GetAllEmployeesQuery(), cancellationToken);
-			if(!employees.Any(x => x.Id == model.EmployeeId))
-			{
-				string message = $"Employee with id {model.EmployeeId} not found";
-				errorMessages.Add(message);
-			}
+            IList<Employee> employees = await _employeesQueryHandler.HandleAsync(new GetAllEmployeesQuery(), cancellationToken);
+            if (!employees.Any(x => x.Id == model.EmployeeId))
+            {
+                string message = $"Employee with id {model.EmployeeId} not found";
+                errorMessages.Add(message);
+            }
 
-			Regex yearRegex = new("[12]\\d{3}");
-			if (!yearRegex.IsMatch(model.Year.ToString()))
-			{
-				string message = $"{nameof(model.Year)} is invalid";
-				errorMessages.Add(message);
-			}
+            var year = employees.Where(e => e.Id == model.EmployeeId).SelectMany(e => e.Parameters);
+            if (!year.Any(y => y.Parameter.Year == model.Year))
+            {
+                string message = $"Employee does not have a record for year {model.EmployeeId}";
+                errorMessages.Add(message);
+            }
+
+            Regex yearRegex = new("[12]\\d{3}");
+            if (!yearRegex.IsMatch(model.Year.ToString()))
+            {
+                string message = $"{nameof(model.Year)} is invalid";
+                errorMessages.Add(message);
+            }
 
             _logger.LogInformation($"End class {nameof(CalculateSalaryRequestModelValidator)} and method {nameof(CalculateSalaryRequestModelValidator.Validate)}");
             if (errorMessages.Any())
-			{
-				string message = string.Join(Environment.NewLine, errorMessages);
-				throw new NotFoundException(message);
-			}
-		}
-	}
+            {
+                string message = string.Join(Environment.NewLine, errorMessages);
+                throw new NotFoundException(message);
+            }
+        }
+    }
 }
